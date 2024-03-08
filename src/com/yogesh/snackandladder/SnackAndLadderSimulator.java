1e1e0884fc38dad7985b6a4c812d5c49923f6e04
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-08 19:15:25 +0530

    [ADDED] : Yogesh Snack and Ladder Simulator Setup

---
+++ src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
@@ -0,0 +1,11 @@
+package com.yogesh.snackandladder;
+
+public class SnackAndLadderSimulator {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome To Snack and Ladder Simulator");
+
+	}
+
+}
