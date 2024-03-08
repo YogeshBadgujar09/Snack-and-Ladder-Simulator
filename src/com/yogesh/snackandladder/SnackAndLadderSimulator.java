84dfd07819e1a6ebef52d3fdc55b51475091ea17
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-08 19:38:41 +0530

    [ADDED] : Yogesh Start game At 0 position

--- src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
+++ src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
@@ -1,10 +1,19 @@
 package com.yogesh.snackandladder;
 
 public class SnackAndLadderSimulator {
+	
+	private static final int START_POSITION = 0 ;
+	
+	public static void gamePlay()
+	{
+		System.out.println("Starting Position :" + START_POSITION);
+	}
 
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome To Snack and Ladder Simulator");
+		
+		gamePlay();
 
 	}
 
