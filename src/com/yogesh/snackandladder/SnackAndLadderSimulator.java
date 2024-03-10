bcddfe669e7e87b7373b9db4f13ce06da38403a6
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-10 23:55:16 +0530

    [MODIFIED] : Yogesh Player is rolls Die

--- src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
+++ src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
@@ -1,5 +1,7 @@
 package com.yogesh.snackandladder;
 
+import java.util.Random;
+
 public class SnackAndLadderSimulator {
 	
 	private int player_position = 0 ;
@@ -8,13 +10,22 @@ public class SnackAndLadderSimulator {
 	{
 		System.out.println("player Position :" + player_position );
 	}
-
+	
+	public void playGame()
+	{
+		Random random = new Random();
+		int die = random.nextInt(6)+1;
+		
+		System.out.println("Die No :" + die);
+	}
+	
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome To Snack and Ladder Simulator");
 		
 		SnackAndLadderSimulator snackandladdersimulator = new SnackAndLadderSimulator();
-		 snackandladdersimulator.showPosition();
+		snackandladdersimulator.showPosition();
+		snackandladdersimulator.playGame();
 	}
 
 }
