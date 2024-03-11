ec4952531864db6825a89dde63fc3356bd19bb46
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-11 15:09:09 +0530

    [MODIFIED] : Yogesh - Check for option : Noplay ,snack Or Ladder

--- src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
+++ src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 public class SnackAndLadderSimulator {
 	
-	private int player_position = 0 ;
+	private int player_position = 0 ; 
+	private static final int NO_PLAY_CASE = 0 ;
+	private static final int LADDER_CASE = 1 ;
+	private static final int SNACK_CASE = 2 ;
 	
 	public void showPosition()
 	{
@@ -13,10 +16,41 @@ public class SnackAndLadderSimulator {
 	
 	public void playGame()
 	{
-		Random random = new Random();
-		int die = random.nextInt(6)+1;
+		int die ;
+		int option ;
 		
+		Random random = new Random();
+		die = random.nextInt(6)+1;
 		System.out.println("Die No :" + die);
+		
+		option = random.nextInt(3);
+		
+		switch(option)
+		{
+			case NO_PLAY_CASE :
+				System.out.println("No PLAY");
+			break ;
+			
+			case LADDER_CASE :
+				System.out.println("LADDER");
+				player_position += die ;
+			break ;
+			
+			case SNACK_CASE :
+				System.out.println("SNACK");
+				player_position -= die ;
+			break ;
+		}
+		
+		
+			int Starting_position = 0;
+	
+			if(player_position <= Starting_position ) {
+				System.out.println("player position : " +  Starting_position );	
+			}
+			else {
+				System.out.println("player position :" + player_position);
+			}
 	}
 	
 	public static void main(String[] args) {
