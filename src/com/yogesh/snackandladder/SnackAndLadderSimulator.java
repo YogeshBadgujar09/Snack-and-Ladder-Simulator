a0565198ce85e74bf40552b51ee2745b4669e6a4
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-14 15:48:45 +0530

    [MODIFIED] : Yogesh - reach win position and restart from 0

--- src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
+++ src/com/yogesh/snackandladder/SnackAndLadderSimulator.java
@@ -4,53 +4,63 @@ import java.util.Random;
 
 public class SnackAndLadderSimulator {
 	
-	private int player_position = 0 ; 
+	private static int player_position = 0 ;
 	private static final int NO_PLAY_CASE = 0 ;
 	private static final int LADDER_CASE = 1 ;
 	private static final int SNACK_CASE = 2 ;
+	private static final int WINNING_POSITION = 100 ;
+	
 	
 	public void showPosition()
 	{
-		System.out.println("player Position :" + player_position );
+		System.out.println("Player Position :" + player_position + "\n" );
 	}
 	
 	public void playGame()
 	{
-		int die ;
-		int option ;
+		showPosition();
 		
-		Random random = new Random();
-		die = random.nextInt(6)+1;
-		System.out.println("Die No :" + die);
+		int player_position = 0 ; 
+		final int RESTART_POSITION = 0;
 		
-		option = random.nextInt(3);
-		
-		switch(option)
-		{
-			case NO_PLAY_CASE :
-				System.out.println("No PLAY");
-			break ;
+		while(player_position != WINNING_POSITION ) {
 			
-			case LADDER_CASE :
-				System.out.println("LADDER");
-				player_position += die ;
-			break ;
-			
-			case SNACK_CASE :
-				System.out.println("SNACK");
-				player_position -= die ;
-			break ;
+				int die ;
+				int option ;				
+				
+				Random random = new Random();
+				die = random.nextInt(6)+1;
+				System.out.println("Die No :" + die);
+				
+				option = random.nextInt(3);
+				
+				switch(option)
+				{
+	 					case NO_PLAY_CASE :
+	 						System.out.println("No PLAY");
+	 						break ;
+					
+	 					case LADDER_CASE :
+	 						System.out.println("LADDER");
+	 						player_position += die ;
+	 					break ;
+					
+	 					case SNACK_CASE :
+	 						System.out.println("SNACK");
+	 						player_position -= die ;
+						
+	 						if(player_position < RESTART_POSITION ) {
+	 							player_position = RESTART_POSITION ;
+	 						}
+	 					break ;
+				}
+				
+				System.out.println("Player Position : " + player_position);
+				System.out.println("...............");
 		}
-		
-		
-			int Starting_position = 0;
-	
-			if(player_position <= Starting_position ) {
-				System.out.println("player position : " +  Starting_position );	
-			}
-			else {
-				System.out.println("player position :" + player_position);
-			}
+				
+			
+			
 	}
 	
 	public static void main(String[] args) {
@@ -58,7 +68,6 @@ public class SnackAndLadderSimulator {
 		System.out.println("Welcome To Snack and Ladder Simulator");
 		
 		SnackAndLadderSimulator snackandladdersimulator = new SnackAndLadderSimulator();
-		snackandladdersimulator.showPosition();
 		snackandladdersimulator.playGame();
 	}
 
